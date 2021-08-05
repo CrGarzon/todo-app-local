037dc1c50484c8d02d9e31902442ab877feec64a
Cristian <cristiangarzonortiz@gmail.com>
2021-08-04 20:19:25 -0500

    Metodos de consulta

--- Backend/todo/src/main/java/com/msc/todo/repositories/TaskRepository.java
+++ Backend/todo/src/main/java/com/msc/todo/repositories/TaskRepository.java
@@ -1,5 +1,8 @@
 package com.msc.todo.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,5 @@ import com.msc.todo.entities.Task;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
-
+	List<Task> findByName(String name);
 }
