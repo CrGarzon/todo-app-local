856ca62d671c20a195ff1643f2310ec7dba2db91
Cristian <cristiangarzonortiz@gmail.com>
2021-08-23 19:28:29 -0500

    proyecto base

--- Frontend/todo-app/src/app/app.component.ts
+++ Frontend/todo-app/src/app/app.component.ts
@@ -6,24 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
-  nombre: string="Cristian";
-  nombreGuardar: string="";
-  edad : number=25;
-  casado : boolean = false;
-  listaNombres : Array<string> = ['cristian','pedro','pepe'];
-  //listaNombres: string[]=[];
-
-  persona = {
-    name: "crg",
-    age: 24,
-    married: false,
-  }
-
-  guardar (){
-    this.nombreGuardar=this.nombre
-    this.listaNombres.push(this.nombre);
-  }
+ 
 
 
 
